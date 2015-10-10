0b2d63cd7ae56741b94aeb2e8714ba7e14980f84
xhnsworks <xhnsworks@gmail.com>
2015-10-11 07:58:17 +0800

    加入Mac和iOS两个版本

---
+++ Mac/xhnSTL/xhnSTL.h
@@ -0,0 +1,19 @@
+//
+//  xhnSTL.h
+//  xhnSTL
+//
+//  Created by 徐海宁 on 15/10/11.
+//  Copyright © 2015年 徐 海宁. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for xhnSTL.
+FOUNDATION_EXPORT double xhnSTLVersionNumber;
+
+//! Project version string for xhnSTL.
+FOUNDATION_EXPORT const unsigned char xhnSTLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <xhnSTL/PublicHeader.h>
+
+
---
+++ Mac/xhnSTLTests/xhnSTLTests.m
@@ -0,0 +1,39 @@
+//
+//  xhnSTLTests.m
+//  xhnSTLTests
+//
+//  Created by 徐海宁 on 15/10/11.
+//  Copyright © 2015年 徐 海宁. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface xhnSTLTests : XCTestCase
+
+@end
+
+@implementation xhnSTLTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
