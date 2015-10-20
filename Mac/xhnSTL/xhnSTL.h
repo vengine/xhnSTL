cba77c2d7c9be36dec5b2075f8b9ddc45c4d5744
haining <xhn.xu@mochang.net>
2015-10-20 17:34:42 +0800

    修复一个编译bug

--- Mac/xhnSTL/xhnSTL.h
+++ Mac/xhnSTL/xhnSTL.h
@@ -5,7 +5,7 @@
 //  Created by 徐海宁 on 15/10/11.
 //  Copyright © 2015年 徐 海宁. All rights reserved.
 //
-
+#ifdef __OBJC__
 #import <Cocoa/Cocoa.h>
 
 //! Project version number for xhnSTL.
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char xhnSTLVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <xhnSTL/PublicHeader.h>
 
 
+#endif
