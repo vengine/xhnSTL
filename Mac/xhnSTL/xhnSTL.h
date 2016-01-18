fd31016efea11406ea2c93b17c69b1420b52acd8
haining <xhn.xu@mochang.net>
2016-01-18 17:41:20 +0800

    在xhnSTL.h里添加import

--- Mac/xhnSTL/xhnSTL.h
+++ Mac/xhnSTL/xhnSTL.h
@@ -16,5 +16,70 @@ FOUNDATION_EXPORT const unsigned char xhnSTLVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <xhnSTL/PublicHeader.h>
 
+#import <xhnSTL/common.h>
+#import <xhnSTL/etypes.h>
+#import <xhnSTL/array.h>
+#import <xhnSTL/eassert.h>
+#import <xhnSTL/elog.h>
+#import <xhnSTL/emem.h>
+#import <xhnSTL/emem_allocator.h>
+#import <xhnSTL/esignal.h>
+#import <xhnSTL/estring.h>
+#import <xhnSTL/exception.h>
+#import <xhnSTL/hash.h>
+#import <xhnSTL/list.h>
+#import <xhnSTL/list_base.h>
+#import <xhnSTL/mem.h>
+#import <xhnSTL/prime.h>
+#import <xhnSTL/rwbuffer.h>
+#import <xhnSTL/sha256.h>
+#import <xhnSTL/spin_lock.h>
+#import <xhnSTL/stack.h>
+#import <xhnSTL/static_string.h>
+#import <xhnSTL/tree.h>
+#import <xhnSTL/utility.h>
+
+#import <xhnSTL/rtti.hpp>
+#import <xhnSTL/xhn_atomic_operation.hpp>
+#import <xhnSTL/xhn_btree.hpp>
+#import <xhnSTL/xhn_cloned_ptr.hpp>
+#import <xhnSTL/xhn_config.hpp>
+#import <xhnSTL/xhn_crypto.hpp>
+#import <xhnSTL/xhn_exception.hpp>
+#import <xhnSTL/xhn_file_stream.hpp>
+#import <xhnSTL/xhn_filesystem_node.h>
+#import <xhnSTL/xhn_fixed_queue.hpp>
+#import <xhnSTL/xhn_functor.hpp>
+#import <xhnSTL/xhn_garbage_collector.hpp>
+#import <xhnSTL/xhn_gc_array.hpp>
+#import <xhnSTL/xhn_gc_string.hpp>
+#import <xhnSTL/xhn_gc_values.hpp>
+#import <xhnSTL/xhn_hash_map.hpp>
+#import <xhnSTL/xhn_hash_set.hpp>
+#import <xhnSTL/xhn_iterator.hpp>
+#import <xhnSTL/xhn_lambda.hpp>
+#import <xhnSTL/xhn_list.hpp>
+#import <xhnSTL/xhn_list2.h>
+#import <xhnSTL/xhn_list_template.hpp>
+#import <xhnSTL/xhn_lock.hpp>
+#import <xhnSTL/xhn_map.hpp>
+#import <xhnSTL/xhn_memory.hpp>
+#import <xhnSTL/xhn_operation.hpp>
+#import <xhnSTL/xhn_operation_manager.hpp>
+#import <xhnSTL/xhn_pair.hpp>
+#import <xhnSTL/xhn_path_tree.hpp>
+#import <xhnSTL/xhn_rbtree.hpp>
+#import <xhnSTL/xhn_recurise.hpp>
+#import <xhnSTL/xhn_routine_pool.hpp>
+#import <xhnSTL/xhn_set.hpp>
+#import <xhnSTL/xhn_smart_ptr.hpp>
+#import <xhnSTL/xhn_static_string.hpp>
+#import <xhnSTL/xhn_string.hpp>
+#import <xhnSTL/xhn_thread.hpp>
+#import <xhnSTL/xhn_thread_map.hpp>
+#import <xhnSTL/xhn_thread_pool.hpp>
+#import <xhnSTL/xhn_utility.hpp>
+#import <xhnSTL/xhn_vector.hpp>
+#import <xhnSTL/xhn_void_vector.hpp>
 
 #endif
