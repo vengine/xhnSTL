a479552bf14c51afabefd426660f8ffe52889f29
xhnsworks <xhnsworks2@icloud.com>
2016-11-14 11:15:34 +0800

    添加单元测试

---
+++ Linux/UnitTest/build.sh
@@ -0,0 +1,6 @@
+source /home/parallels/Projects/SDK/environment-setup-cortexa9hf-vfp-neon-poky-linux-gnueabi
+rm -rf CMakeFiles
+rm CMakeCache.txt
+rm cmake_install.cmake
+rm Makefile
+cmake . -DCMAKE_TOOLCHAIN_FILE=OEToolchainConfig.cmake
---
+++ Linux/UnitTest/test.cpp
@@ -0,0 +1,21 @@
+#include <xhnSTL/common.h>
+#include <xhnSTL/etypes.h>
+#include <xhnSTL/xhn_lock.hpp>
+#include <xhnSTL/xhn_thread.hpp>
+int main(int argc, char *argv[])
+{
+    printf("hello world\n");
+    xhn::thread_ptr t = VNEW xhn::thread();
+    while(!t->is_running()) {}
+    volatile bool completed = false;
+
+    xhn::Lambda<xhn::thread::TaskStatus()> proc([&completed, &tp](){
+        printf("hello world from other thread!\n");
+        completed = true;
+        return xhn::thread::Completed;
+    });
+
+    t->add_lambda_task(proc);
+    while (!completed) {}
+    return 0;
+}
