604d4e44ef1dfedf900d28de0638ecba364de400
xhnsworks <xhnsworks@gmail.com>
2020-01-24 22:05:40 +0800

    实现linux和windows版本的number_of_cores和number_of_physicalcores

--- xhnSTL/xlibc/cpu_common.c
+++ xhnSTL/xlibc/cpu_common.c
@@ -71,7 +71,78 @@ euint32 number_of_physicalcores()
     
     return ncores;
 }
+#elif defined(_WIN32) || defined(_WIN64)
+#include <windows.h>
+euint32 number_of_cores()
+{
+    euint32 registers[4];
+    unsigned logicalcpucount;
+    unsigned physicalcpucount;
 
+    SYSTEM_INFO systeminfo;
+    GetSystemInfo( &systeminfo );
+
+    logicalcpucount = systeminfo.dwNumberOfProcessors;
+    return logicalcpucount;
+}
+euint32 number_of_physicalcores()
+{
+    euint32 registers[4];
+    unsigned logicalcpucount;
+    unsigned physicalcpucount;
+
+    SYSTEM_INFO systeminfo;
+    GetSystemInfo( &systeminfo );
+
+    logicalcpucount = systeminfo.dwNumberOfProcessors;
+    __asm__ __volatile__ ("cpuid " :
+                          "=a" (registers[0]),
+                          "=b" (registers[1]),
+                          "=c" (registers[2]),
+                          "=d" (registers[3])
+                          : "a" (1), "c" (0));
+    unsigned CPUFeatureSet = registers[3];
+    bool hyperthreading = CPUFeatureSet & (1 << 28);
+    if (hyperthreading){
+        physicalcpucount = logicalcpucount / 2;
+    } else {
+        physicalcpucount = logicalcpucount;
+    }
+    return physicalcpucount;
+}
+#elif defined(LINUX)
+#include <unistd.h>
+euint32 number_of_cores()
+{
+    euint32 registers[4];
+    unsigned logicalcpucount;
+    unsigned physicalcpucount;
+
+    logicalcpucount = sysconf( _SC_NPROCESSORS_ONLN );
+    return logicalcpucount;
+}
+euint32 number_of_physicalcores()
+{
+    euint32 registers[4];
+    unsigned logicalcpucount;
+    unsigned physicalcpucount;
+
+    logicalcpucount = sysconf( _SC_NPROCESSORS_ONLN );
+    __asm__ __volatile__ ("cpuid " :
+                          "=a" (registers[0]),
+                          "=b" (registers[1]),
+                          "=c" (registers[2]),
+                          "=d" (registers[3])
+                          : "a" (1), "c" (0));
+    unsigned CPUFeatureSet = registers[3];
+    bool hyperthreading = CPUFeatureSet & (1 << 28);
+    if (hyperthreading){
+        physicalcpucount = logicalcpucount / 2;
+    } else {
+        physicalcpucount = logicalcpucount;
+    }
+    return physicalcpucount;
+}
 #else
 
 euint32 number_of_cores()
